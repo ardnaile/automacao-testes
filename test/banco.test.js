072abd6c57b3d69a1088afe2ef9f1f8fa1a14f4a
Eliandra Motta Cardoso <eliandra.282421@alunosatc.edu.br>
2025-03-13 20:04:54 -0300

    test: adiciona testes para funcao depositar

--- test/banco.test.js
+++ test/banco.test.js
@@ -1,7 +1,31 @@
 const Banco = require("../src/banco");
 
-describe("Testes da classe Banco", () => {
-  let conta;
+describe("Banco", () => {
+  let banco;
 
-  test("testes", async () => {});
+  beforeEach(() => {
+    banco = new Banco(); // Cria uma nova instância da classe antes de cada teste
+  });
+
+  // test("testes", async () => {});
+
+  test("Depositar o valor alterando o saldo corretamente", async () => {
+    const valorDepositado = 50;
+  
+    const saldoAposDeposito = banco.depositar(valorDepositado);
+  
+    expect(saldoAposDeposito).toBe(50);
+  });
+  
+  test("Adicionar o depósito na lista de transações", async () => {
+    banco.depositar(50);
+  
+    expect(banco.transacoes).toHaveLength(1);
+    expect(banco.transacoes[0]).toEqual({ tipo: 'Depósito', valor: 50 });
+  });
+
+  test("Sacar valor maior que o saldo", async () => {
+    
+  })
 });
+
