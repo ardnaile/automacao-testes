1565e780290360840cbb858dbca2f23df3df8ff4
ardnaile <elimottacardoso@gmail.com>
2025-03-14 15:25:22 -0300

    test: adiciona testes para os métodos sacar, obterSaldo e obterHistorico

--- test/banco.test.js
+++ test/banco.test.js
@@ -4,17 +4,17 @@ describe("Banco", () => {
   let banco;
 
   beforeEach(() => {
-    banco = new Banco(); // Cria uma nova instância da classe antes de cada teste
+    banco = new Banco(); 
+    banco.saldo = 50;
   });
 
-  // test("testes", async () => {});
-
+  // Método 1
   test("Depositar o valor alterando o saldo corretamente", async () => {
     const valorDepositado = 50;
   
     const saldoAposDeposito = banco.depositar(valorDepositado);
   
-    expect(saldoAposDeposito).toBe(50);
+    expect(saldoAposDeposito).toBe(100);
   });
   
   test("Adicionar o depósito na lista de transações", async () => {
@@ -24,8 +24,26 @@ describe("Banco", () => {
     expect(banco.transacoes[0]).toEqual({ tipo: 'Depósito', valor: 50 });
   });
 
-  test("Sacar valor maior que o saldo", async () => {
-    
+  // Método 2
+  test("Sacar valor válido e atualizar o saldo", async () => {
+    const novoSaldo = banco.sacar(20); // sacar retorna o novo saldo
+    expect(novoSaldo).toBe(30);
+    expect(banco.transacoes).toContainEqual({ tipo: 'Saque', valor: 20 });
+  })
+
+  // Método 3
+
+  // Método 4
+  test("Obter saldo atual", async () => {
+    expect(banco.saldo).toBe(50);
+  })
+
+  // Método 5
+  test("Obter histórico de transações", async () => {
+    banco.depositar(20);
+    banco.sacar(20);
+    expect(banco.transacoes[0]).toEqual({ tipo: 'Depósito', valor: 20 });
+    expect(banco.transacoes[1]).toEqual({ tipo: 'Saque', valor: 20 });
   })
 });
 
